f854bd2aebe887752a48662c3e6982bb3203f22d
T-Meneses <thiagomenese@gmail.com>
2024-08-29 21:30:12 -0300

    Add files via upload

---
+++ whilebart.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <header>
+
+    </header>
+    <body>
+<?php        
+
+ $mensagem = 'Estou aprendendo loopings no Aprender PHP';
+        $i = 1;
+         While ($i <=100) {
+            echo $i.'-'.$mensagem . "<br>".PHP_EOL;
+            $i++;
+        }
+?>
+</body>
+
+
+</html>
